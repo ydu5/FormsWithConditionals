d4d0fe6f5ec311c6442dc597cbf6cd4abdc5e0b5
ydu5 <LYD@Lucys-MacBook-Pro.local>
2015-10-15 11:01:45 -0400

    complete

--- run.rb
+++ run.rb
@@ -9,6 +9,23 @@ end
 
 get '/form' do
   # code!
+@states = []
+state = {id: 'IN', name: 'Indiana'}
+@states << state
+
+state = {id: 'MI', name: 'Michigan'}
+@states << state
+
+state = {id: 'NJ', name: 'New Jersey'}
+@states << state
+
+state = {id: 'NY', name: 'New York'}
+@states << state
+
+state = {id: 'AZ', name: 'Arizona'}
+@states << state
+
+@states.sort_by! {|state| state[:name]}
   erb :form, layout: :main
 end
 
@@ -27,20 +44,3 @@ post '/example' do
   erb :example, layout: :main
 end
 
-@states = []
-state = {id: 'IN', name: 'Indiana'}
-@states << state
-
-state = {id: 'MI', name: 'Michigan'}
-@states << state
-
-state = {id: 'NJ', name: 'New Jersey'}
-@states << state
-
-state4 = {id: 'NY', name: 'New York'}
-@states << state
-
-state = {id: 'AZ', name: 'Arizona'}
-@states << state
-
-@states.sort_by! {|state| state[:name]}
